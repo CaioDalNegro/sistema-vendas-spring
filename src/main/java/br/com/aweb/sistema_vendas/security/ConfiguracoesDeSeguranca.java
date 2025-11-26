74cb1e4cf46e8f2331d66898b74937831131a642
CaioDalNegro <caiodalnegro@gmail.com>
2025-11-26 19:19:19 -0300

    NEW: Subindo details com usuario

--- src/main/java/br/com/aweb/sistema_vendas/security/ConfiguracoesDeSeguranca.java
+++ src/main/java/br/com/aweb/sistema_vendas/security/ConfiguracoesDeSeguranca.java
@@ -4,13 +4,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class ConfiguracoesDeSeguranca {
+
+    @Bean
+    public UserDetailsService dadosUsuarios() {
+        UserDetails usuario1 = User.builder()
+                .username("teste@gmail.com")
+                .password("123")
+                .build();
+
+        return new InMemoryUserDetailsManager(usuario1);
+
+    }
 
     @Bean
     public SecurityFilterChain filtrosSeguranca(HttpSecurity http) throws Exception {
@@ -24,12 +39,10 @@ public class ConfiguracoesDeSeguranca {
                 .formLogin(form -> form
                         .loginPage("/login")
                         .defaultSuccessUrl("/", true)
-                        .permitAll()
-                )
+                        .permitAll())
                 .logout(logout -> logout
                         .logoutSuccessUrl("/login?logout")
-                        .permitAll()
-                )
+                        .permitAll())
                 .rememberMe(r -> r.key("lembrarDeMim"))
                 .build();
     }
